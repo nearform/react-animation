6b35d52682340b5f2e267c0f3c8e7ca529ddfd51
Julian Goacher <julian.goacher@innerfunction.com>
2019-06-18 09:22:23 +0100

    Removed obsolete code.

--- src/AnimateOnChange/index.js
+++ src/AnimateOnChange/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, /*useLayoutEffect, */useState, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 import { animations } from '../theme'
 import '../theme/keyframes.css'
